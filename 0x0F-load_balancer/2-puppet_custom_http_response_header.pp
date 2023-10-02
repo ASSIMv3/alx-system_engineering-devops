c05b2d1af9671d99c6adb63732b0ad178919a865
ASSIMv3 <hassanassim01@gmail.com>
2023-10-02 12:42:38 -0700

    adv task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,22 @@
+# Installs a Nginx server with custome HTTP header
+
+exec {'update':
+  command  => 'sudo apt-get -y update',
+  provider => shell,
+  before   => Exec['install_nginx'],
+}
+
+exec {'install_nginx':
+  command  => 'sudo apt-get -y install nginx',
+  provider => shell,
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  command => '/bin/sed -i "/^\tlocation \/ {.*/a \\\t \tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default; /usr/sbin/service nginx restart'
+}
+
+exec { 'nginx_restart':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
