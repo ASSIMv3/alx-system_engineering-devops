4763175e28dfe023d8ee93b0690d3a0020497f92
ASSIMv3 <hassanassim01@gmail.com>
2023-11-07 04:20:43 -0800

    mandatory task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,18 +5,17 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     """returns a list of titles"""
-
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
-    
+
     if after:
         url += f'&after={after}'
-    
+
     headers = {'User-Agent': 'API-Client'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         posts = response.json()['data']['children']
-   
+
         for post in posts:
             title = post['data']['title']
             hot_list.append(title)
