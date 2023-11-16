ed6ac5748f38feee5f25f1589274a236c98a0710
ASSIMv3 <hassanassim01@gmail.com>
2023-11-16 07:02:07 -0800

    mandatory task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,10 @@
 # login with the holberton user and open a file without any error message
-exec { 'increase limit':
+exec {'hard_increase':
   provider => shell,
   command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 50000/g" /etc/security/limits.conf',
 }
-exec { 'increase soft':
+
+exec {'soft_increase':
   provider => shell,
-  command  => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 40000/g" /etc/security/limits.conf', 
+  command  => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 40000/g" /etc/security/limits.conf',
 }
